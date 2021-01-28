72baeecc4bfe6cc50750d425deffefcd98c47420
woshilaiceshide <woshilaiceshide@qq.com>
2021-01-28 12:00:21 +0800

    add some request condition in AdvancedHttpServer

--- src/test/scala/woshilaiceshide/sserver/test/AdvancedHttpServer.scala
+++ src/test/scala/woshilaiceshide/sserver/test/AdvancedHttpServer.scala
@@ -5,6 +5,7 @@ import woshilaiceshide.sserver.nio._
 import spray.http._
 import spray.http.HttpEntity.apply
 import spray.http.StatusCode.int2StatusCode
+import spray.http.Uri.Path.{Segment, Slash}
 
 /**
  * A http server with websocket, chunked request, chunked response, plain http and so on.
@@ -16,6 +17,7 @@ object AdvancedHttpServer extends App {
   val log = org.slf4j.LoggerFactory.getLogger(AdvancedHttpServer.getClass);
 
   import scala.concurrent._
+
   val executor = java.util.concurrent.Executors.newSingleThreadExecutor()
   implicit val ec = ExecutionContext.fromExecutor(executor)
 
@@ -38,56 +40,86 @@ object AdvancedHttpServer extends App {
         }
 
         override def idled(): Unit = {}
-        def pongReceived(frame: WebSocket13.WSFrame): Unit = { println("ping ok") }
+
+        def pongReceived(frame: WebSocket13.WSFrame): Unit = {
+          println("ping ok")
+        }
+
         def fireClosed(code: WebSocket13.CloseCode.Value, reason: String): Unit = {}
-        def inputEnded(): Unit = { c.close(WebSocket13.CloseCode.NORMAL_CLOSURE_OPTION) }
+
+        def inputEnded(): Unit = {
+          c.close(WebSocket13.CloseCode.NORMAL_CLOSURE_OPTION)
+        }
+
         def channelWritable(): Unit = {}
       }
 
     }
 
     private val ping = new HttpResponse(200, HttpEntity(ContentTypes.`text/plain`, "Hello, World!"))
+
     private def write_ping(channel: HttpChannel) = {
       ResponseAction.responseWithThis(ping, size_hint = 128, false)
     }
+
     private val path_ping = Uri.Path("/ping")
 
     private def write_404(channel: HttpChannel) = {
-      channel.writeResponse { new HttpResponse(404) }
+      channel.writeResponse {
+        new HttpResponse(404)
+      }
       ResponseAction.responseByMyself
     }
 
     private def write_400(channel: HttpChannel) = {
-      channel.writeResponse { new HttpResponse(400) }
+      channel.writeResponse {
+        new HttpResponse(400)
+      }
       ResponseAction.responseByMyself
     }
 
     private val ping_asynchronously = new HttpResponse(200, HttpEntity(ContentTypes.`text/plain`, "Hello World Asynchronously"))
+
     private def write_ping_asynchronously(channel: HttpChannel) = {
       //channel.post_to_io_thread { channel.writeResponse(ping_asynchronously) }
-      Future { channel.writeResponse(ping_asynchronously) }
+      Future {
+        channel.writeResponse(ping_asynchronously)
+      }
       ResponseAction.responseByMyself
     }
 
-    private def accept_websocket(request: HttpRequest) = ResponseAction.acceptWebsocket { websocket(request) }
+    private def accept_websocket(request: HttpRequest) = ResponseAction.acceptWebsocket {
+      websocket(request)
+    }
 
     private def count_0123456789_with_x_times(channel: HttpChannel, times: Int) = {
 
       ResponseAction.acceptChunking(new ChunkedRequestHandler() {
 
-        def channelIdled(): Unit = { /*do nothing*/ }
-        def channelWritable(): Unit = { /*do nothing*/ }
-        def channelClosed(): Unit = { /*do nothing*/ }
+        def channelIdled(): Unit = {
+          /*do nothing*/
+        }
+
+        def channelWritable(): Unit = {
+          /*do nothing*/
+        }
+
+        def channelClosed(): Unit = {
+          /*do nothing*/
+        }
 
         private var finished = false
+
         private def format_error() = {
           finished = true
           channel.writeResponse(new HttpResponse(400, "request entity must in format: 01234567890123456789 ...\r\n"))
         }
+
         private def ok() = {
           finished = true
           channel.writeResponse(new HttpResponse(200, s"${times} of 0123456789 are received correctly\r\n"))
         }
+
         private def times_error() = {
           finished = true
           channel.writeResponse(new HttpResponse(400, s"${times} of 0123456789 are expected, but ${already} of ... is received\r\n"))
@@ -95,6 +127,7 @@ object AdvancedHttpServer extends App {
 
         private var already: Int = 0
         private var last_digit: Byte = -1
+
         private def eat_a_digit(d: Byte) = {
           if (last_digit == -1) {
             if (0 == d) {
@@ -122,15 +155,22 @@ object AdvancedHttpServer extends App {
 
         def chunkReceived(chunk: MessageChunk): Unit = {
           val bytes = chunk.data.toByteArray
-          log.debug(s"receve from ${channel.remoteAddress}: ${bytes.map { _.toString }.mkString}")
+          log.debug(s"receive from ${channel.remoteAddress}: ${
+            bytes.map {
+              _.toString
+            }.mkString
+          }")
+
           @scala.annotation.tailrec def eat(i: Int): Unit = {
             if (i < bytes.length && !finished) {
               eat_a_digit(bytes(i))
               eat(i + 1)
             }
           }
+
           eat(0)
         }
+
         def chunkEnded(end: ChunkedMessageEnd): Unit = {
           if (!finished) {
             if (already == times) ok() else times_error()
@@ -140,23 +180,27 @@ object AdvancedHttpServer extends App {
     }
 
     private def do_not_support_chunked_request(channel: HttpChannel) = {
-      println("do not support chunked")
-      channel.writeResponse { new HttpResponse(400, "I DOES NOT support chunked request.") }
+      println("chunked request is NOT supported")
+      channel.writeResponse {
+        new HttpResponse(400, "chunked request is NOT supported")
+      }
       ResponseAction.responseByMyself
     }
 
     //wrk -c100 -t2 -d30s -H "Connection: keep-alive" -H "User-Agent: ApacheBench/2.4" -H "Accept: */*"  http://127.0.0.1:8787/ping
     def requestReceived(request: HttpRequest, channel: HttpChannel, classifier: RequestClassifier): ResponseAction = request match {
 
-      case HttpRequest(HttpMethods.GET, uri, _, _, _) if uri.path == path_ping     => write_ping(channel)
+      case HttpRequest(HttpMethods.GET, uri, _, _, _) if uri.path == path_ping => write_ping(channel)
 
       //wrk -c100 -t2 -d30s --script=./scripts/pipeline_ping.lua http://127.0.0.1:8787/ping_asynchronously
       case HttpRequest(HttpMethods.GET, Uri.Path("/ping_asynchronously"), _, _, _) => write_ping_asynchronously(channel)
 
-      case x @ HttpRequest(HttpMethods.GET, Uri.Path("/websocket"), _, _, _)       => accept_websocket(x)
+      case x@HttpRequest(HttpMethods.GET, Uri.Path("/websocket"), _, _, _)
+        if classifier.classification(x) == RequestClassification.WebsocketStart => accept_websocket(x)
 
-      case HttpRequest(HttpMethods.POST, uri, _, _, _) if uri.path.startsWith(Uri.Path("/chunked_request")) => {
-        log.debug(s"a new request targeted at ${uri} from ${channel.remoteAddress}")
+      case x@HttpRequest(HttpMethods.POST, uri, _, _, _)
+        if classifier.classification(x) == RequestClassification.ChunkedHttpStart => {
+        log.debug(s"a new request targeted at ${x.uri} from ${channel.remoteAddress}")
         import Uri.Path._
         uri.path match {
           case Slash(Segment("chunked_request", Slash(Segment(times, _)))) =>
@@ -176,7 +220,8 @@ object AdvancedHttpServer extends App {
         ResponseAction.responseByMyself
       }
 
-      case x: HttpRequest if classifier.classification(x) == RequestClassification.ChunkedHttpStart => do_not_support_chunked_request(channel)
+      case x: HttpRequest
+        if classifier.classification(x) == RequestClassification.ChunkedHttpStart => do_not_support_chunked_request(channel)
 
       case _: HttpRequest => write_404(channel)
     }
